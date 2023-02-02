9ac4da926826b693208e341c9e78d7fb78c0a7b0
codesafe <codesafe@gmail.com>
2023-02-02 21:59:34 +0900

    Bootloader 성공

---
+++ assemble.bat
@@ -0,0 +1,6 @@
+nasm boot.asm -f bin -o os.flp
+qemu-system-x86_64 -drive format=raw,file=os.flp
+pause
+
+
+
---
+++ boot.asm
@@ -0,0 +1,34 @@
+; https://www.youtube.com/watch?v=Hc5EiQ2GsrI
+; https://www.youtube.com/watch?v=48dFP4PBHII
+
+; boot loader BIOS magic number
+org 0x7c00
+bits 16
+
+start:
+    mov si, stringtest
+    mov bx, 0
+
+loop:    
+    ;lodsb
+    mov al, [si]
+    cmp al, 0
+    je done
+    call printf
+    inc si
+    jmp loop
+done
+    jmp $
+
+printf:
+    mov ah, 0x0e
+    int 0x10
+    ret
+
+
+stringtest: 
+    db '#### Welcome to My Bootloader !! -_- ####', 0
+
+times 510-($-$$) db 0     ; fill the rest of the sector ( 처음 510 바이트 ) with 0
+dw 0xAA55                   ; boot signature
+
